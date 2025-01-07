91b5e68fd10d5e957acf3cdb289a69755e76eaca
Lucia Soria <luciacsoria5@gmail.com>
2025-01-07 18:03:10 -0300

    Add sobreMi.js

--- src/components/SobreMi.jsx
+++ src/components/SobreMi.jsx
@@ -1,9 +1,18 @@
 import { Box, Grid, Typography, useMediaQuery, useTheme } from '@mui/material';
 import React from 'react';
+import { descripcion } from '../info/sobreMi';
 
 export const SobreMi = () => {
   const theme = useTheme();
   const isSmallScreen = useMediaQuery(theme.breakpoints.down('md')); // Detecta pantallas pequeñas
+
+  const desc = descripcion;
+
+  var cont = 0;
+  const getCont = () => {
+    cont = cont + 1;
+    return cont;
+  }
 
   return (
     <Box
@@ -36,18 +45,12 @@ export const SobreMi = () => {
             <Typography variant='h2' component='div' gutterBottom>
               Sobre mí
             </Typography>
-            <Typography variant='body1' fontSize={20}>
-              Soy una estudiante de Sistemas con una gran pasión por la tecnología y el desarrollo de software. Soy segura de mí misma, responsable, organizada, y siempre me enfoco en alcanzar mis objetivos.
-              <br />
-              Con determinación y creatividad, logré no sólo sobresalir en mis estudios, sino también disfrutar cada paso del camino.
-              <br />
-              Tengo sólidos conocimientos en programación, bases de datos, y testing. Además, gracias a mi experiencia, puedo comunicar ideas de manera efectiva, tanto en proyectos individuales como en equipo.
-              Siempre busco sumar al grupo, ya sea aportando ideas o ayudando a otros a alcanzar su máximo potencial.
-              <br />
-              Estoy convencida de que la clave del éxito está en la combinación de esfuerzo y pasión, y eso es algo que aplico en todo lo que hago.
-              <br />
-              Estoy emocionada por lo que el futuro me depara, y estoy lista para seguir creciendo y aprendiendo.
-            </Typography>
+            {desc.map(parrafo => (
+              <Typography variant='body1' fontSize={20} key={ getCont() }>
+                {parrafo}
+              </Typography>
+            ))}
+
           </Box>
         </Grid>
 
