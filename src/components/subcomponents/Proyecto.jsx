ffb11aea9407081c4e4ea271533a4b4ed627e0c8
Lucia Soria <luciacsoria5@gmail.com>
2025-01-09 21:09:42 -0300

    Fix proyecto: make responsive

--- src/components/subcomponents/Proyecto.jsx
+++ src/components/subcomponents/Proyecto.jsx
@@ -10,6 +10,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSquareGitlab } from '@fortawesome/free-brands-svg-icons'
 import LanguageIcon from '@mui/icons-material/Language';
 import { useMediaQuery } from '@mui/material';
+import { Block } from '@mui/icons-material';
 
 export const Proyecto = ({ proyecto }) => {
 
@@ -17,97 +18,213 @@ export const Proyecto = ({ proyecto }) => {
   const isSmallScreen = useMediaQuery(theme.breakpoints.down('md')); // Detecta pantallas pequeñas
 
   const { titulo, herramientas, descripcion, repositorio, imagen, sitio = '', git } = proyecto;
-  
+
   var textoHerramientas = '';
-  herramientas.forEach( herr => {
+  herramientas.forEach(herr => {
     textoHerramientas += '#' + herr + ' '.repeat(5);
   });
 
   const ImgUrl = `./proyectos/${imagen}`;
-  
-  return (
-    <Card
-      sx={{
-        display: 'flex',
-        width: '100%',  // El ancho será controlado por el `Grid`
-        height: 300,    // Fija la altura
-        overflowY: 'auto',
-      }}
-    >
-      <CardMedia
-        component="img"
-        sx={{
-          width: 'auto',
-          height: '100%',
-          objectFit: 'cover' /* Imagen consistente */
-        }}
-        image={ImgUrl}
-        alt={titulo}
-      />
-      <Box
-        sx={{
-          display: 'flex',
-          height: '100%',          // Asegura que el contenedor ocupe todo el espacio
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}>
 
-        <Box
+  return (
+    <>
+      {isSmallScreen
+        ?
+        (
+          <Card
+            sx={{
+              // flexDirection: 'column', 
+              display: 'block',          // Usa flexbox para manejar los iconos
+              alignItems: 'center',     // Centra los iconos horizontalmente
+              width: '100%',  // El ancho será controlado por el `Grid`
+              height: 300,    // Fija la altura
+              overflowY: 'auto',
+              overflowX: 'auto',
+            }}
+          >
+
+            <CardContent
+              sx={{
+                alignSelf: 'stretch', // Ocupa todo el ancho
+              }}
+            >
+              <Typography component="div" variant="h5">
+                {titulo}
+              </Typography>
+              <Typography variant="body1" component="div" fontSize={20} mb={2}>
+                {descripcion}
+              </Typography>
+              {/* <Block
+                // sx={{
+                //   display: 'block',
+                //   justifyContent: 'center',
+                // }}
+              > */}
+              <Typography
+                variant="body1"
+                component="div"
+                fontSize={15}
+                // style={{ whiteSpace:'pre'}}
+                sx={{
+                  display: 'block', // Hace que el Typography sea un bloque
+                  width: '100%', // Hace que ocupe todo el ancho disponible
+                  wordWrap: 'break-word', // Permite que las palabras largas se dividan en la siguiente línea
+                  whiteSpace: 'normal', // Permite que el texto se divida en varias líneas
+                }}
+              >
+                {textoHerramientas}
+              </Typography>
+              {/* </Block> */}
+            </CardContent>
+
+            <Box
+              sx={{
+                display: 'block',
+                height: '100%',          // Asegura que el contenedor ocupe todo el espacio
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}>
+              <Box
+                sx={{
+                  flexDirection: 'row',  // Apila los iconos verticalmente
+                  display: 'block',          // Usa flexbox para manejar los iconos
+                  alignItems: 'center',     // Centra los iconos horizontalmente
+                  gap: 1,                   // Espaciado entre los iconos
+                  pl: 1,
+                  pb: 1,
+                  pr: 3,
+                }}>
+                {
+                  sitio && (
+                    <IconButton
+                      role="button"
+                      variant="contained"
+                      onClick={() => window.open(sitio, "_blank", "noopener noreferrer")}
+                    >
+                      <LanguageIcon fontSize="large" />
+                    </IconButton>
+                  )
+                }
+                <IconButton
+                  role="button"
+                  variant="contained"
+                  onClick={() => window.open(repositorio, "_blank", "noopener noreferrer")}
+                >
+                  {git === "GitHub" ? (
+                    <GitHubIcon fontSize="large" />
+                  )
+                    : (
+                      <FontAwesomeIcon
+                        icon={faSquareGitlab}
+                        size="xl"
+                        style={{ color: "#808080" }}
+                      />
+                    )}
+                </IconButton>
+              </Box>
+              <CardMedia
+                component="img"
+                sx={{
+                  width: '100%',
+                  height: 'auto',
+                  objectFit: 'cover' /* Imagen consistente */
+                }}
+                image={ImgUrl}
+                alt={titulo}
+              />
+            </Box>
+          </Card>
+        )
+
+
+
+        :
+        <Card
           sx={{
-            display: 'flex',          // Usa flexbox para manejar los iconos
-            flexDirection: 'column',  // Apila los iconos verticalmente
-            alignItems: 'center',     // Centra los iconos horizontalmente
-            gap: 1,                   // Espaciado entre los iconos
-            pl: 1,
-            pb: 1,
-            pr: 3,
-          }}>
-          {
-            sitio && (
+            display: 'flex',
+            width: '100%',  // El ancho será controlado por el `Grid`
+            height: 300,    // Fija la altura
+            overflowY: 'auto',
+            overflowX: 'auto',
+          }}
+        >
+          <CardMedia
+            component="img"
+            sx={{
+              width: 'auto',
+              height: '100%',
+              objectFit: 'cover' /* Imagen consistente */
+            }}
+            image={ImgUrl}
+            alt={titulo}
+          />
+          <Box
+            sx={{
+              display: 'flex',
+              height: '100%',          // Asegura que el contenedor ocupe todo el espacio
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+
+            <Box
+              flexDirection={isSmallScreen ? 'row' : 'column'}  // Apila los iconos verticalmente
+              sx={{
+                display: 'flex',          // Usa flexbox para manejar los iconos
+                alignItems: 'center',     // Centra los iconos horizontalmente
+                gap: 1,                   // Espaciado entre los iconos
+                pl: 1,
+                pb: 1,
+                pr: 3,
+              }}>
+              {
+                sitio && (
+                  <IconButton
+                    role="button"
+                    variant="contained"
+                    onClick={() => window.open(sitio, "_blank", "noopener noreferrer")}
+                  >
+                    <LanguageIcon fontSize="large" />
+                  </IconButton>
+                )
+              }
               <IconButton
                 role="button"
                 variant="contained"
-                onClick={() => window.open(sitio, "_blank", "noopener noreferrer")}
+                onClick={() => window.open(repositorio, "_blank", "noopener noreferrer")}
               >
-                <LanguageIcon fontSize="large" />
+                {git === "GitHub" ? (
+                  <GitHubIcon fontSize="large" />
+                ) : (
+                  <FontAwesomeIcon
+                    icon={faSquareGitlab}
+                    size="xl"
+                    style={{ color: "#808080" }}
+                  />
+                )}
               </IconButton>
-            )
-          }
-          <IconButton
-            role="button"
-            variant="contained"
-            onClick={() => window.open(repositorio, "_blank", "noopener noreferrer")}
-          >
-            {git === "GitHub" ? (
-              <GitHubIcon fontSize="large" />
-            ) : (
-              <FontAwesomeIcon
-                icon={faSquareGitlab}
-                size="xl"
-                style={{ color: "#808080" }}
-              />
-            )}
-          </IconButton>
-        </Box>
+            </Box>
 
-        <CardContent
-          sx={{
-            alignSelf: 'stretch', // Ocupa todo el ancho
-          }}
-        >
-          <Typography component="div" variant="h5">
-            {titulo}
-          </Typography>
-          <Typography variant="body1" component="div" fontSize={20} mb={2}>
-            {descripcion}
-          </Typography>
-          <Typography variant="body1" component="div" fontSize={15} style={{ whiteSpace: 'pre' }}>          
-            { textoHerramientas }
-          </Typography>
-          
-        </CardContent>
+            <CardContent
 
-      </Box>
-    </Card>
+              sx={{
+                alignSelf: 'stretch', // Ocupa todo el ancho
+              }}
+            >
+              <Typography component="div" variant="h5">
+                {titulo}
+              </Typography>
+              <Typography variant="body1" component="div" fontSize={20} mb={2}>
+                {descripcion}
+              </Typography>
+              <Typography variant="body1" component="div" fontSize={15} style={{ whiteSpace: 'pre' }}>
+                {textoHerramientas}
+              </Typography>
+
+            </CardContent>
+
+          </Box>
+        </Card>
+      }
+    </>
   );
 }
