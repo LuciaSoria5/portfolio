fbd4613e89d59b7af5407d3f69de56c7e230951e
Lucia Soria <luciacsoria5@gmail.com>
2025-01-15 15:41:37 -0300

    Add background images

--- src/pages/MiPortfolio.jsx
+++ src/pages/MiPortfolio.jsx
@@ -1,5 +1,8 @@
-import { Contacto, Navbar, Presentacion, Proyectos, SobreMi, Tecnologias } from '../components'
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import { Box } from '@mui/material'
+import { Contacto, Navbar, Presentacion, Proyectos, SobreMi, Tecnologias } from '../components'
 
 import estrellasRosas from '/fondos/estrellasRosas.gif';
 
@@ -7,16 +10,30 @@ import brillosRosas1 from '/fondos/brillosRosas1.jpg';
 import brillosRosas2 from '/fondos/brillosRosas2.jpg';
 import brillos from '/fondos/brillos.jpg';
 import espacio from '/fondos/espacio.jpg';
+import colores from '/fondos/colores.jpg';
+import azul from '/fondos/azul.jpg';
+import violeta from '/fondos/violeta.jpg';
+import fucsia from '/fondos/fucsia.jpg';
 import estrellas from '/fondos/estrellas.jpg';
 import rosa from '/fondos/rosa.jpg';
+import rosaVerde from '/fondos/rosaVerde.jpg';
+import rosaVioleta from '/fondos/rosaVioleta.jpg';
 
 
-const imgNavbar = brillosRosas1;
-const imgPresentacion = estrellasRosas;
-const imgFondo = brillosRosas1;
-const imgContacto = brillosRosas2;
+const imgNavbar = violeta;
+// const imgPresentacion = brillosRosas1;
+const imgFondo = rosaVioleta;
+// const imgContacto = brillosRosas2;
 
 export const MiPortfolio = () => {
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1200, // Duración en ms
+      once: false,     // Ejecutar la animación solo una vez
+      mirror: false,
+    });
+  }, []);
 
   return (
     <Box
@@ -31,69 +48,51 @@ export const MiPortfolio = () => {
         backgroundColor: 'primary.main',
         color: 'primary.color',
       }}
+      style={{
+        backgroundImage: `url(${imgFondo})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
     >
       <Navbar
         styleImg={
-          { 
-            backgroundImage: `url(${imgNavbar})`, 
-            // backgroundColor: '#000000'
+          {
+            backgroundImage: `url(${imgNavbar})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
           }
         } />
       <section
         className="animate__animated animate__fadeIn"
         id="presentacion"
-        style={{
-          // height: '100vh',
-          backgroundImage: `url(${imgPresentacion})`,
-          // backgroundRepeat: "no-repeat",
-          // backgroundSize: "cover",
-        }}
       >
         <Presentacion /> </section>
 
       <section
-        className="animate__animated animate__fadeInLeft"
+        data-aos="fade-up"
+        // className="animate__animated animate__fadeInLeft"
         id="sobre-mi"
-        style={{
-          // height: '100vh',
-          backgroundImage: `url(${imgFondo})`,
-          // backgroundRepeat: "no-repeat",
-          // backgroundSize: "cover",
-        }}>
+      >
         <SobreMi id="sobre-mi" /> </section>
 
       <section
-        className="animate__animated animate__fadeInRight"
+        data-aos="fade-up"
+        // className="animate__animated animate__fadeInRight"
         id="mas-detalles"
-        style={{
-          // height: '100vh',
-          backgroundImage: `url(${imgFondo})`,
-          // backgroundRepeat: "no-repeat",
-          // backgroundSize: "cover",
-        }}>
+      >
         <Tecnologias id="tecnologias" /> </section>
 
       <section
-        className="animate__animated animate__fadeInLeft"
+        data-aos="fade-up"
+        // className="animate__animated animate__fadeInLeft"
         id="proyectos"
-        style={{
-          // height: '100vh',
-          backgroundImage: `url(${imgFondo})`,
-          // backgroundRepeat: "no-repeat",
-          // backgroundSize: "cover",
-        }}>
+      >
         <Proyectos id="proyectos" /> </section>
 
       <section
-        className="animate__animated animate__fadeIn"
+        // className="animate__animated animate__fadeIn"
         id="contacto"
-        style={{
-          // height: '100vh',
-          backgroundImage: `url(${imgContacto})`,
-          // backgroundPosition: 'left bottom',
-          // backgroundRepeat: "no-repeat",
-          // backgroundSize: "cover",
-        }}>
+      >
         <Contacto id="contacto" /> </section>
 
     </Box>
